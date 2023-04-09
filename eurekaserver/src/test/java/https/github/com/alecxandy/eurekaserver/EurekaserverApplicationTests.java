bac898c33f27de24913163c334f22bcb16e8395c
alecxandy <alecxandy@gmail.com>
2023-04-09 02:03:47 -0300

    Criação e Configuração do Discovery Server

---
+++ eurekaserver/src/test/java/https/github/com/alecxandy/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
