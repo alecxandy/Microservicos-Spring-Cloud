69a64ac928be091678d44dce6de115ef7dc2edb1
alecxandy <alecxandy@gmail.com>
2023-04-24 22:40:42 -0300

    Configuração do gatway

--- mscloudgateway/src/main/java/https/github/com/alecxandy/MicroservicosSpringCloud/git/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/https/github/com/alecxandy/MicroservicosSpringCloud/git/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,24 @@ package https.github.com.alecxandy.MicroservicosSpringCloud.git.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
 public class MscloudgatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MscloudgatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MscloudgatewayApplication.class, args);
+    }
+
+    public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder
+                .routes()
+                .route(r -> r.path("/cliente/**").uri("lb://msclientes")).build();
+    }
 
 }
