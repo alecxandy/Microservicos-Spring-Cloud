8d4c6a0d0e0d06143185f2840c32dfc581df68c4
alecxandy <alecxandy@gmail.com>
2023-04-24 21:36:57 -0300

    Inicio do projeto mscloudgatway

---
+++ mscloudgateway/src/test/java/https/github/com/alecxandy/MicroservicosSpringCloud/git/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.MicroservicosSpringCloud.git.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
