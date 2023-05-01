883d363a982d2d5223180671fc5c996b9405d730
alecxandy <alecxandy@gmail.com>
2023-05-01 02:33:08 -0300

    Iniciando projeto mscartoes

---
+++ mscartoes/src/main/java/https/github/com/alecxandy/MicroservicosSpringCloud/git/mscartoes/MscartoesApplication.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.MicroservicosSpringCloud.git.mscartoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscartoesApplication.class, args);
+	}
+
+}
---
+++ mscartoes/src/test/java/https/github/com/alecxandy/MicroservicosSpringCloud/git/mscartoes/MscartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.MicroservicosSpringCloud.git.mscartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
