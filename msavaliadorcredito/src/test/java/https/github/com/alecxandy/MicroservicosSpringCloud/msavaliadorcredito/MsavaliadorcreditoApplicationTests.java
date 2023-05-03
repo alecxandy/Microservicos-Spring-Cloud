8be9bbc28b7c8a7dd286411576777dfdc469bc3b
alecxandy <alecxandy@gmail.com>
2023-05-02 22:11:44 -0300

    Inicio do projeto msvalidadorcredito

---
+++ msavaliadorcredito/src/test/java/https/github/com/alecxandy/MicroservicosSpringCloud/msavaliadorcredito/MsavaliadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.alecxandy.MicroservicosSpringCloud.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
